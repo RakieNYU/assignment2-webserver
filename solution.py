0b620d123fc269d9538308eedcd99a56a0fb09b6
Rakienyu <rv2226@nyu.edu>
2022-02-21 11:17:52 -0600

    submission

--- solution.py
+++ solution.py
@@ -1,53 +1,46 @@
-#RakieNYU-2
+#RakieNYU
 # import socket module
 from socket import *
 # In order to terminate the program
 import sys
 
-
 def webServer(port=13331):
+  host = "127.0.0.1"
   serverSocket = socket(AF_INET, SOCK_STREAM)
   #Prepare a server socket
-  serverSocket.bind(("", port))
+  serverSocket.bind((host, port))
   #Fill in start
-
+  serverSocket.listen(1)
   #Fill in end
-
   while True:
     #Establish the connection
     #print('Ready to serve...')
-    connectionSocket, addr = #Fill in start      #Fill in end
+    connectionSocket, addr = serverSocket.accept()
     try:
-
       try:
-        message = #Fill in start    #Fill in end
+        message = connectionSocket.recv(1024).decode()
         filename = message.split()[1]
         f = open(filename[1:])
-        outputdata = #Fill in start     #Fill in end
-        
+        outputdata = f.read()
         #Send one HTTP header line into socket.
         #Fill in start
-
+        connectionSocket.send(("HTTP/1.1 200 OK\r\n\r\n").encode())
         #Fill in end
-
         #Send the content of the requested file to the client
         for i in range(0, len(outputdata)):
           connectionSocket.send(outputdata[i].encode())
-
         connectionSocket.send("\r\n".encode())
         connectionSocket.close()
       except IOError:
         # Send response message for file not found (404)
-        #Fill in start
-
+        # Fill in start
+        connectionSocket.send(("HTTP/1.1 404 Not Found\r\n\r\n").encode())
+        connectionSocket.send("<html><head></head><body><h2>Page Not Found</h2></body></html>".encode())
+        # Fill in end
+        # Close client socket
+        # Fill in start
+        connectionSocket.close()
         #Fill in end
-
-
-        #Close client socket
-        #Fill in start
-
-        #Fill in end
-
     except (ConnectionResetError, BrokenPipeError):
       pass
 
